5f252bc007629bcaf4751a92f845a24d8cffae41
mibmasa <sazan_masa@yahoo.co.jp>
2018-07-01 16:22:05 +0900

    未使用箇所の修正に伴い、使いやすいよう一部を修正。なおほかの箇所も修正しているため後々置き換えする。

--- autoUnitTestsCommander2/MainWindow.xaml.cs
+++ autoUnitTestsCommander2/MainWindow.xaml.cs
@@ -21,19 +21,22 @@ namespace autoUnitTestsCommander2
     /// </summary>
     public partial class MainWindow : Window
     {
+        string VSTest = @"""C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\IDE\CommonExtensions\Microsoft\TestWindow\\vstest.console.exe""";
+
         public MainWindow()
         {
             InitializeComponent();
-            /*TODO:
-             * VSTest.Console.exeが存在するディレクトリがわからないため
-             * 起動時にあるかを確認する条件を追加する
-             */
-            string VSTest = @"VSTest.Console.exe";
+            pasetButton.IsEnabled = false;
             //if (!System.IO.File.Exists(VSTest))
             //{
             //    this.Close();
             //}
         }
+        /// <summary>
+        /// UnitTestDll
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void openButton_Click(object sender, RoutedEventArgs e)
         {
             var dllOpen = new OpenFileDialog();
@@ -44,46 +47,193 @@ namespace autoUnitTestsCommander2
                 this.unitTestDirectryText.Text = dllOpen.FileName;
             }
         }
-
+        /// <summary>
+        /// ClipboardPaset
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void pasetButton_Click(object sender, RoutedEventArgs e)
         {
-            string vsTestConsole = @"";
-            string unitTestsDirectryToStrong = "";
-            string tests = "";
-            string testFilter = "";
-            string logger = "";
-            string lingString = "";
-            if (0 == NullChecker(unitTestDirectryText.Text))
+            string lingString = VSTest;
+
+            if ("" != unitTestDirectryText.Text)
             {
-                unitTestsDirectryToStrong = unitTestDirectryText.Text;
+                lingString = lingString + " " + @"""" + unitTestDirectryText.Text + @"""" + " ";
             }
-            if (0 == NullChecker(testFilterText.Text))
+            if ("" != SettingsText.Text)
             {
-                tests = testsText.Text;
+                lingString += @"""/Settings:" + SettingsText.Text + @"""" + " ";
             }
-            if (0 == NullChecker(loggerText.Text))
+            if ("" != loggerText.Text)
             {
-                logger = loggerText.Text;
+                lingString += @"""/logger:" + loggerText.Text + @"""" + " ";
             }
-            if (0 == NullChecker(testFilterText.Text))
+            if ("" != TestCaseFilterText.Text)
             {
-                testFilter = testFilterText.Text;
+                lingString += @"""/TestCaseFilter:" + @"""" + TestCaseFilterText.Text + @"""" + @"""" + " ";
             }
-            lingString = vsTestConsole + unitTestsDirectryToStrong + tests + testFilter + logger;
+            if ("" != testsText.Text)
+            {
+                lingString += @"""/Tests:" + testsText.Text + @"""" + " ";
+            }
+            if (InIsolationText.IsEnabled)
+            {
+                lingString += @"""" + InIsolationText.Text + @"""" + " ";
+            }
+            if (EnablecodecoverageText.IsEnabled)
+            {
+                lingString += @"""" + EnablecodecoverageText.Text + @"""" + " ";
+            }
+            if (UseVsixExtensionsText.IsEnabled)
+            {
+                lingString += @"""" + UseVsixExtensionsText.Text + @"""" + " ";
+            }
+            if (PlatformText.IsEnabled)
+            {
+                lingString += @"""" + PlatformText.Text + @"""" + " ";
+            }
+            if (FrameworkText.IsEnabled)
+            {
+                lingString += @"""" + FrameworkText.Text + @"""" + " ";
+            }
 
             Clipboard.SetData(DataFormats.Text, (Object)lingString);
-
+        }
+        /// <summary>
+        /// SettingOpen
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void SettingButton_Click(object sender, RoutedEventArgs e)
+        {
+            var settingOpen = new OpenFileDialog();
+            settingOpen.Title = "testsettings Open";
+            settingOpen.Filter = "*.testsettings,*.runsettings|*.testsettings,*.runsettings";
+            if (true == settingOpen.ShowDialog())
+            {
+                this.unitTestDirectryText.Text = settingOpen.FileName;
+            }
         }
 
-        public int NullChecker(string checkString)
+        private void InIsolationCheck_Click(object sender, RoutedEventArgs e)
         {
-            if (null == checkString)
+            string fixCharactor = "/InIsolation:";
+            if (InIsolationCheck.IsChecked == true)
             {
-                return -1;
+                if (!fixCharactor.Equals(InIsolationText.Text))
+                {
+                    InIsolationText.Text += fixCharactor;
+                }
+                InIsolationText.IsEnabled = true;
             }
             else
             {
-                return 0;
+                InIsolationText.Text = "";
+                InIsolationText.IsEnabled = false;
+            }
+        }
+
+        private void EnablecodecoverageCheck_Click(object sender, RoutedEventArgs e)
+        {
+            string fixCharactor = "/Enablecodecoverage:";
+            if (EnablecodecoverageCheck.IsChecked == true)
+            {
+                if (!fixCharactor.Equals(EnablecodecoverageText.Text))
+                {
+                    EnablecodecoverageText.Text += fixCharactor;
+                }
+                EnablecodecoverageText.IsEnabled = true;
+            }
+            else
+            {
+                EnablecodecoverageText.Text = "";
+                EnablecodecoverageText.IsEnabled = false;
+            }
+        }
+
+        private void UseVsixExtensionsCheck_Click(object sender, RoutedEventArgs e)
+        {
+            string fixCharactor = "/UseVsixExtensions:";
+            if (UseVsixExtensionsCheck.IsChecked == true)
+            {
+                if (!fixCharactor.Equals(UseVsixExtensionsText.Text))
+                {
+                    UseVsixExtensionsText.Text += fixCharactor;
+                }
+                UseVsixExtensionsText.IsEnabled = true;
+            }
+            else
+            {
+                UseVsixExtensionsText.Text = "";
+                UseVsixExtensionsText.IsEnabled = false;
+            }
+        }
+
+        private void PlatformCheck_Click(object sender, RoutedEventArgs e)
+        {
+            string fixCharactor = "/Platform:";
+            if (PlatformCheck.IsChecked == true)
+            {
+                if (!fixCharactor.Equals(PlatformText.Text))
+                {
+                    PlatformText.Text += fixCharactor;
+                }
+                PlatformText.IsEnabled = true;
+            }
+            else
+            {
+                PlatformText.Text = "";
+                PlatformText.IsEnabled = false;
+            }
+        }
+
+        private void FrameworkCheck_Click(object sender, RoutedEventArgs e)
+        {
+            string fixCharactor = "/Framework:";
+            if (FrameworkCheck.IsChecked == true)
+            {
+                if (!fixCharactor.Equals(FrameworkText.Text))
+                {
+                    FrameworkText.Text += fixCharactor;
+                }
+                FrameworkText.IsEnabled = true;
+            }
+            else
+            {
+                FrameworkText.Text = "";
+                FrameworkText.IsEnabled = false;
+            }
+        }
+
+        private void TestCategory_Click(object sender, RoutedEventArgs e)
+        {
+            string fixCharactor = "TestCategory=(Requirement Number)";
+            if (TestCategoryCheck.IsChecked == true)
+            {
+                if (!fixCharactor.Equals(TestCaseFilterText.Text))
+                {
+                    TestCaseFilterText.Text = fixCharactor;
+                }
+            }
+            else
+            {
+                TestCaseFilterText.Text = "";
+            }
+        }
+
+        private void unitTestDirectryText_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (unitTestDirectryText.Text != "" && SettingsText.Text != "")
+            {
+                pasetButton.IsEnabled = true;
+            }
+        }
+
+        private void SettingsText_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (unitTestDirectryText.Text != "" && SettingsText.Text != "")
+            {
+                pasetButton.IsEnabled = true;
             }
         }
     }
